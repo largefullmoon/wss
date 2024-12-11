571f606145dea1efe2931d8d55fe85323149c906
largefullmoon <largefullmoon@gmail.com>
2024-12-11 17:26:25 +0200

    add category filter for getting onging events count

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -841,10 +841,10 @@ const eventCountPerZoneCategory = async () => {
             if (count > 0) {
                 const ongoing_count = await Event.countDocuments({ zone: zone._id, category: category.name, createdAt: { $gte: oneMinutesAgo }, battery_status: 'ongoing' })
                 const resolved_count = await Event.countDocuments({ zone: zone._id, category: category.name, createdAt: { $gte: oneMinutesAgo }, battery_status: 'resolved' })
-                const infoOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#006FEE" })
-                const warningOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#F5A524" })
-                const errorOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#F31260" })
-                const criticalOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "red" })
+                const infoOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#006FEE", category: "issue" })
+                const warningOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#F5A524", category: "issue" })
+                const errorOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "#F31260", category: "issue" })
+                const criticalOngoingCount = await Event.countDocuments({ zone: zone._id, battery_status: "ongoing", color: "red", category: "issue" })
                 const data = {
                     zone: zone._id,
                     category: category.name,
